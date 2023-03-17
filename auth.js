9076ebde8c1ad6b0ae41ddcfa88e2ea83dbfbd0c
Vivek Yadav <vivekyadav36837@gmail.com>
2023-03-17 17:34:53 +0530

    update

--- auth.js
+++ auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const key = process.env.JWT_SECRET_KEY;
+
+
+const key ='Bp2Jj7wMKEKCyaesF4umQSUsN17NT5ZScjN7hxljGTYge97gDtDvDY6wVDSFGBzpZ80a7wEzAFctM5d75g0ONA==';
 
 module.exports = function(req, res, next) {
   // Get token from header
   const token = req.header('Authorization');
-
+  // console.log(token);
   // Check if token is not present
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
@@ -14,7 +15,7 @@ module.exports = function(req, res, next) {
   try {
     // Verify token
     const decoded = jwt.verify(token, key);
-    console.log('Decoded token:', decoded);
+    // console.log('Decoded token:', decoded);
     
     // Set user from payload
     req.user = decoded.user;
--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const User = require('./models/user');
 const Post = require('./models/post');
-const auth  = require('./auth');
+const auth = require('./auth');
+const Comments = require('./models/comment');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 // const cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 
 const app = express();
-// const key = process.env.JWT_SECRET_KEY
+const key = 'Bp2Jj7wMKEKCyaesF4umQSUsN17NT5ZScjN7hxljGTYge97gDtDvDY6wVDSFGBzpZ80a7wEzAFctM5d75g0ONA=='
 // console.log(key);
 app.use(
   bodyParser.urlencoded({
-      extended: false
+    extended: false
   })
 );
 
@@ -26,16 +27,16 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/socialapi';
 
 // // Connect to MongoDB
 mongoose.connect(mongoUrl, { useNewUrlParser: true })
-    .then(() => {
-        console.log('Connected to MongoDB successfully.');
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB', error);
-    });
+  .then(() => {
+    console.log('Connected to MongoDB successfully.');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error);
+  });
 
 
-app.get("/",(req,res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("Hello World");
 })
 
 
@@ -43,14 +44,14 @@ app.get("/",(req,res)=>{
 // APIs ENDPOINTS
 
 // User auth 
-app.post('/api/authenticate', async (req, res) => {
+app.post('/api/auth', async (req, res) => {
   const { email, password } = req.body;
   console.log({ email }, { password });
 
   try {
     // Find user with given email
     const user = await User.findOne({ email });
-    console.log(user);
+    // console.log(user);
 
     // Check if user exists and password is correct
     // if (!user || !await bcrypt.compare(password, user.password)) {
@@ -58,8 +59,8 @@ app.post('/api/authenticate', async (req, res) => {
     // }
 
     // Generate JWT token
-    const token = jwt.sign({ userId: user.id },process.env.JWT_SECRET_KEY, { expiresIn: '24h' });
-    console.log(token);
+    const token = jwt.sign({ userId: user.id }, key, { expiresIn: '24h' });
+    // console.log(token);
 
     // Return token
     res.send({ token });
@@ -72,121 +73,158 @@ app.post('/api/authenticate', async (req, res) => {
 
 
 
-  // User follow 
-  app.post('/api/follow/:id', auth, async (req, res) => {
-    const { id } = req.params;
-  
+// User follow 
+app.post('/api/follow/:id', auth, async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send('Invalid id parameter');
+  }
+
+  try {
     // Add user to following list
     await User.updateOne({ _id: req.userId }, { $addToSet: { following: id } });
-  
+
     // Add user to followers list
     await User.updateOne({ _id: id }, { $addToSet: { followers: req.userId } });
-  
+
     res.send('User followed successfully');
+    console.log('User followed successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+
+
+// User unfollow
+app.post('/api/unfollow/:id', auth, async (req, res) => {
+  const { id } = req.params;
+
+  // Remove user from following list
+  await User.updateOne({ _id: req.userId }, { $pull: { following: id } });
+
+  // Remove user from followers list
+  await User.updateOne({ _id: id }, { $pull: { followers: req.userId } });
+
+  res.send('User unfollowed successfully');
+  console.log('User unfollowed successfully');
+});
+
+//get user detail
+
+app.get('/api/user/:id', auth, async (req, res) => {
+  const { id } = req.params;
+
+  // Get user profile
+  const user = await User.findById(id).select('name followers following');
+  console.log(user)
+  res.send(user);
+});
+
+
+
+// post detail
+app.post('/api/posts', auth, async (req, res) => {
+  const { title, description, author } = req.body;
+
+  // Create new post
+  const post = new Post({
+    title,
+    description,
+    author,
+    createdBy: req.userId,
+    createdAt: new Date(),
   });
 
-  
-  // User unfollow
-  app.post('/api/unfollow/:id', auth, async (req, res) => {
+  await post.save();
+
+  res.send(post);
+});
+
+// delete post 
+app.delete('/api/posts/:id', auth, async (req, res) => {
+  const { id } = req.params;
+
+  // Delete post
+  await Post.findByIdAndDelete(id);
+
+  res.send('Post deleted successfully');
+});
+
+
+
+// post like
+app.post('/api/posts/:id/like', auth, async (req, res) => {
+  const { id } = req.params;
+
+  // Add like to post
+  await Post.updateOne({ _id: id }, { $addToSet: { likes: req.userId } });
+
+  res.send('Post liked successfully');
+});
+
+
+
+// post unlike 
+app.post('/api/posts/:id/unlike', auth, async (req, res) => {
+  const { id } = req.params;
+
+  // Remove like from post
+  await Post.updateOne({ _id: id }, { $pull: { likes: req.userId } });
+
+  res.send('Post unliked successfully');
+});
+
+
+
+// add comment to post
+app.post('/api/posts/:id/comment', auth, async (req, res) => {
+  try {
     const { id } = req.params;
-  
-    // Remove user from following list
-    await User.updateOne({ _id: req.userId }, { $pull: { following: id } });
-  
-    // Remove user from followers list
-    await User.updateOne({ _id: id }, { $pull: { followers: req.userId } });
-  
-    res.send('User unfollowed successfully');
-  });
 
-
-  // get user detail
-  app.get('/api/user', auth, async (req, res) => {
-    // Get user profile
-    const user = await User.findById(req.userId).select('name followers following');
-  
-    res.send(user);
-  });
-
-
-  // post detail
-  app.post('/api/posts', auth, async (req, res) => {
-    const { title, description } = req.body;
-  
-    // Create new post
-    const post = new Post({
-      title,
-      description,
-      createdBy: req.userId,
-      createdAt: new Date(),
-    });
-  
-    await post.save();
-  
-    res.send(post);
-  });
-
-  // delete post 
-  app.delete('/api/posts/:id', auth, async (req, res) => {
-    const { id } = req.params;
-  
-    // Delete post
-    await Post.findByIdAndDelete(id);
-  
-    res.send('Post deleted successfully');
-  });
-
-
-
-  // post like
-  app.post('/api/posts/:id/like', auth, async (req, res) => {
-    const { id } = req.params;
-  
-    // Add like to post
-    await Post.updateOne({ _id: id }, { $addToSet: { likes: req.userId } });
-  
-    res.send('Post liked successfully');
-  });
-
-
-
-  // post unlike 
-  app.post('/api/posts/:id/unlike', auth, async (req, res) => {
-    const { id } = req.params;
-  
-    // Remove like from post
-    await Post.updateOne({ _id: id }, { $pull: { likes: req.userId } });
-  
-    res.send('Post unliked successfully');
-  });
-
-
-
-  // add comment to post
-  app.post('/api/posts/:id/comment', auth, async (req, res) => {
-    const { id } = req.params;
-  
+    // Validate input
+    if (!id) {
+      return res.status(400).send({ message: 'Post ID is required' });
+    }
+    if (!req.body.text) {
+      return res.status(400).send({ message: 'Comment text is required' });
+    }
+    const postId = req.params.id;
     // Add comment to post
-    await Post.updateOne({ _id: id }, { $addToSet: { comments: req.body } });
-  
-    res.send('Comment added successfully');
-  });
+    const post = await Post.findOneAndUpdate(
+      { _id: id },
+      { $addToSet: { comments: { text: req.body.text, user: po } } },
+      { new: true }
+    ).populate('comments.user', 'name email -_id');
+
+    if (!post) {
+      return res.status(404).send({ message: 'Post not found' });
+    }
+
+    res.send({ message: 'Comment added successfully', post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Server Error' });
+  }
+});
 
 
 
-  // delete comment
-  app.delete('/api/posts/:id/comment/:commentId', auth, async (req, res) => {
-    const { id, commentId } = req.params;
-  
-    // Delete comment
-    await Post.updateOne({ _id: id }, { $pull: { comments: { _id: commentId } } });
-  
-    res.send('Comment deleted successfully');
-  });
+// delete comment
+app.delete('/api/posts/:id/comment/:commentId', auth, async (req, res) => {
+  const { id, commentId } = req.params;
+
+  // Delete comment
+  await Post.updateOne({ _id: id }, { $pull: { comments: { _id: commentId } } });
+
+  res.send('Comment deleted successfully');
+});
 
 
-  // get a single post with its number of likes and comments
-  app.get('/api/posts/:id', auth, async (req, res) => {
+// get a single post with its number of likes and comments
+app.get('/api/posts/:id', auth, async (req, res) => {
   const postId = req.params.id;
 
   try {
@@ -221,41 +259,87 @@ app.post('/api/authenticate', async (req, res) => {
   }
 });
 
+//get all posts from the auth user
+
+app.get('/api/all_posts', auth, async (req, res) => {
+  console.log("all posts created");
+  try {
+    const posts = await Post.find({ user: req.user && req.user._id }).populate('likes').populate({
+      path: 'comments',
+      populate: {
+        path: 'user',
+        model: 'User',
+        select: 'name email -_id'
+      }
+    }).sort({ created_at: -1 });
+    const result = posts.map(post => {
+      const numLikes = post.likes.length;
+      const numComments = post.comments.length;
+
+      return {
+        id: post._id,
+        title: post.title,
+        description: post.description,
+        created_at: post.created_at,
+        likes: numLikes,
+        comments: numComments > 0 ? post.comments : []
+      };
+    });
+
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'Server Error' });
+  }
+});
+
+
+
+
+
+
+
+
 
 
 // get all posts created by auth user 
 
-  app.get('/api/all_posts', auth, async (req, res) => {
-    try {
-      const posts = await Post.find({ user: req.user._id }).populate('likes').populate({
-        path: 'comments',
-        populate: {
-          path: 'user',
-          model: 'User',
-          select: 'name email -_id'
-        }
-      }).sort({ created_at: -1 });
-      const result = posts.map(post => {
-        const numLikes = post.likes.length;
-        const numComments = post.comments.length;
-  
-        return {
-          id: post._id,
-          title: post.title,
-          desc: post.desc,
-          created_at: post.created_at,
-          likes: numLikes,
-          comments: numComments > 0 ? post.comments : []
-        };
-      });
-  
-      res.send(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ message: 'Server Error' });
-    }
-  });
-  
+// app.get('/api/all_posts', auth, async (req, res) => {
+//   try {
+//     const posts = await Post.find({ user: req.user._id })
+//       .populate('likes')
+//       .populate({
+//         path: 'comments',
+//         populate: {
+//           path: 'user',
+//           model: 'User',
+//           select: 'name email -_id'
+//         }
+//       })
+//       .sort({ created_at: -1 });
+
+//     const result = posts.map(post => {
+//       const numLikes = post.likes.length;
+//       const numComments = post.comments.length;
+
+//       return {
+//         id: post._id,
+//         title: post.title,
+//         description: post.description,
+//         created_at: post.created_at,
+//         likes: numLikes,
+//         comments: numComments > 0 ? post.comments : []
+//       };
+//     });
+
+//     res.send(result);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({ message: 'Server Error' });
+//   }
+// });
+
+
 
 
 
